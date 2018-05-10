75c488bf3450ab10876d6b5bdaf60825874a7f50
YC <yanqing.cheng@metaswitch.com>
2018-05-10 21:05:22 +0100

    Unstylish button

--- metaswitch_tinder/pages/home.py
+++ metaswitch_tinder/pages/home.py
@@ -6,7 +6,7 @@ from metaswitch_tinder.config_model import MetaswitchTinder
 
 def home(config: MetaswitchTinder):
     return html.Div([
-        dcc.Link('Go to Page 1', href='/page-1'),
+        html.A(html.Button('Page 1', id='button1'), href='/page-1'),
         html.Br(),
         dcc.Link('Go to Page 2', href='/page-2'),
     ])
