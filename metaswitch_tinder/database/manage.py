b04808d5a353b100e47d9688a2f12d2136f20baa
henry-wallis <henry.wallis@metaswitch.com>
2018-05-11 12:19:49 +0100

    Requests (#3)
    * Add database Request table and a couple more functions
    
    * Add database Request table and a couple more functions
    
    * Add database Request table and a couple more functions

--- metaswitch_tinder/database/manage.py
+++ metaswitch_tinder/database/manage.py
@@ -1,4 +1,6 @@
 from metaswitch_tinder.global_config import DATABASE as db
+from random import randint
+import time
 
 class User(db.Model):
     name = db.Column(db.String(80), primary_key=True)
@@ -14,13 +16,52 @@ class User(db.Model):
 
     def __repr__(self):
         return """
-            Name - %s
-              Email - %s
-              
-              Bio - %s
-              
-              Tags - %s
-            """ % (self.name, self.email, self.bio, self.tags)
+        Name - %s
+          Email - %s      
+          Bio - %s    
+          Tags - %s
+        """ % (self.name, self.email, self.bio, self.tags)
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+        return
+
+class Request(db.Model):
+    """
+    id - Randomly generated "unique" ID of request
+    maker - Name of user who made the request (str)
+    tags - Comma separated list of tags
+    comment - Comment about request (str)
+    state - The state of the request, should be
+            * unmatched (no match found yet)
+            * pending (email sent to potential match)
+            * matched (a match has been accepted)
+    """
+    id = db.Column(db.String, primary_key = True)
+    maker = db.Column(db.String(80))
+    tags = db.Column(db.String(2000))
+    comment = db.Column(db.String(2000))
+    state = db.Column(db.String(80))
+
+    def __init__(self, maker, tags, comment, state):
+        self.id = str(time.time()) + str(randint(1, 100))
+        self.maker = maker
+        self.tags = tags
+        self.comment = comment
+        self.state = state
+
+        if isinstance(self.tags, list):
+            self.tags = ",".join(self.tags)
+
+    def __repr__(self):
+        return """
+        ID - %s
+        Maker - %s
+        Tags - %s
+        State - %s
+        Comment - %s
+        """ % (self.id, self.maker, self.tags, self.state, self.comment)
 
     def add(self):
         db.session.add(self)
@@ -34,10 +75,21 @@ def list_all_users():
 def get_user(match_name):
     return User.query.filter_by(name=match_name).first()
 
+def get_request_by_user(match_name):
+    # Return list of all Requests made by "match_name"
+    return Request.query.filter_by(maker=match_name).all()
+
+def purge_table(table):
+    # Delete all rows in specified table
+    table.query.delete()
+    db.session.commit()
+    return
+
 def delete_table(table):
+    # Delete specified table
     table.__table__.drop(db.session.bind)
     db.session.commit()
     return
 
-db.create_all()
+#db.create_all()
 
--- metaswitch_tinder/pages/mentor_menu.py
+++ metaswitch_tinder/pages/mentor_menu.py
@@ -1,6 +1,17 @@
 from metaswitch_tinder.config_model import MetaswitchTinder
 from metaswitch_tinder.components.tabs import generate_tabs
-from metaswitch_tinder.database.manage import User, list_all_users, get_user
-
+from metaswitch_tinder.database.manage import User, list_all_users, get_user, Request, get_request_by_user, delete_table, purge_table
 def mentor_menu(config: MetaswitchTinder):
     return generate_tabs({'Messages': 'messages', 'Matches': 'matches'}, 'matches')
+
+x = Request("Henry", ["sip", "rtp"], "I want lern stuff", "unmatched")
+
+purge_table(Request)
+
+x.add()
+
+hits = get_request_by_user("Henry")
+
+for x in hits:
+    print(x)
+
