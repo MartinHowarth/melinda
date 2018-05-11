16d8160998c82172c8b72698e4b5d677df3284d4
YC <yanqing.cheng@metaswitch.com>
2018-05-11 12:22:25 +0100

    Improving mentee landing page

--- metaswitch_tinder/pages/mentee_landing_page.py
+++ metaswitch_tinder/pages/mentee_landing_page.py
@@ -18,7 +18,7 @@ def mentee_landing_page(config: MetaswitchTinder):
     session['is_mentee'] = True
     if 'username' in session:
         is_signed_in_fields = [
-            html.H4("Welcome {}!".format(session['username']),
+            html.H3("Welcome {}!".format(session['username']),
                     className="text-center"),
             # Must include something with the id `email-NAME`, but hidden in this case
             dcc.Input(value='', type='text', id='email-{}'.format(NAME), style={'display': 'none'}),
@@ -26,10 +26,7 @@ def mentee_landing_page(config: MetaswitchTinder):
         ]
     else:
         is_signed_in_fields = [
-            html.A(html.Button("I have an account!",
-                               id='sign-in-{}'.format(NAME),
-                               className="btn btn-lg btn-primary btn-block"),
-                   href='/mentee-signin'),
+            html.H3('Existing Users', className="text-center"),
             html.Br(),
             create_equal_row([
                 html.Label('Name:', className="text-center"),
@@ -40,6 +37,12 @@ def mentee_landing_page(config: MetaswitchTinder):
                 html.Label('Email:', className="text-center"),
                 dcc.Input(value='@metaswitch.com', type='text', id='email-{}'.format(NAME)),
             ]),
+            html.Br(),
+            html.A(html.Button("Sign in",
+                               id='sign-in-{}'.format(NAME),
+                               className="btn btn-lg btn-primary btn-block"),
+                   href='/mentee-signin'),
+            html.Br(),
         ]
 
     return html.Div([
@@ -47,17 +50,19 @@ def mentee_landing_page(config: MetaswitchTinder):
         html.Br(),
         *is_signed_in_fields,
         html.Br(),
-        html.H4('What do you want to learn about?', className="text-center"),
+        html.H2('New mentoring request', className="text-center"),
+        html.Br(),
+        html.H4('Select your mentoring topic', className="text-center"),
         html.Br(),
         multi_dropdown_with_tags(database.tags.get_tags(), 'categories-{}'.format(NAME)),
         html.Br(),
-        html.H4('What specifically about that?', className="text-center"),
+        html.H4('Additional topic tags', className="text-center"),
         html.Br(),
         create_equal_row([dcc.Input(value='', type='text', id='details-{}'.format(NAME))]),
         html.Br(),
-        html.A(html.Button("Submit!",
+        html.A(html.Button("Submit my request!",
                            id='submit-{}'.format(NAME),
-                           className="btn btn-lg btn-primary btn-block"),
+                           className="btn btn-lg btn-success btn-block"),
                href='/mentee-menu')
     ],
         className="container", id='my-div')
