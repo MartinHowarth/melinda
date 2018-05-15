daceb865d2b573127d606cb8d704c5d13f0d7471
Martin Howarth <MartinHowarth@users.noreply.github.com>
2018-05-15 21:46:52 +0100

    Test review apps (#32)
    * test
    
    * fix shebang
    
    * try fix deploy script
    
    * remove nonsense

--- metaswitch_tinder/populate_test_database.py
+++ metaswitch_tinder/populate_test_database.py
@@ -26,3 +26,10 @@ def populate():
             "Teach me a random thing.",
         )
         req.add()
+
+
+if __name__ == "__main__":
+    # This is run as part of the heroku pipelines for staging apps.
+    # The live app doesn't run this - it uses the environment DATABASE_URL to connect
+    # to the postgres service provided by heroku.
+    populate()
